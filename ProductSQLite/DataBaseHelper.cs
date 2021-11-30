69ec451851bbec932c3ffbcd0ddc270fc92df7d4
ak-parser <81529142+ak-parser@users.noreply.github.com>
2021-11-30 19:48:38 +0200

    Complited task

---
+++ ProductSQLite/DataBaseHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+
+namespace ProductSQLite
+{
+    internal static class DataBaseHelper
+    {
+        public static List<Product> GetProductList(this DataBase dataBase)
+        {
+            return dataBase.GetList("Select * from Products", Product.Create);
+        }
+
+        public static Product GetProductByName(this DataBase dataBase, string name)
+        {
+            return dataBase.GetList($"Select * from Products Where Name Like '{name}' Limit 1", Product.Create)?.FirstOrDefault();
+        }
+
+        public static Product GetProductByNameLinq(this DataBase dataBase, string name)
+        {
+            return dataBase.GetProductList().Where(product => product.Name.Equals(name, StringComparison.OrdinalIgnoreCase)).FirstOrDefault();
+        }
+    }
+}
---
+++ ProductSQLite/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ProductSQLite;
+
+DataBase dataBase = new DataBase("Data Source=product.db");
+
+Console.WriteLine("Product list: ");
+foreach (var elem in dataBase.GetProductList())
+    Console.WriteLine(elem);
+
+Console.Write("Enter product name: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Result: ");
+Console.WriteLine(dataBase.GetProductByName(name));
+Console.WriteLine("With Linq: " + dataBase.GetProductByNameLinq(name));
